f7bee4b14de883d319dff69c35eb78904c5437b7
Gopesh <medayilgopesh@gmail.com>
2017-02-14 12:43:21 +0530

    Test

--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,33 @@ gulp.task('connect', function() {
 });
 
 gulp.task('default', ['connect']);
+
+[{
+	"Reference": "Registration",
+	"key": "REG_LBL",
+	"English": "Company Registration",
+	"Japanese": "会社登録"
+}, {
+	"Reference": "Registration",
+	"key": "INFO_LBL",
+	"English": "Company Information",
+	"Japanese": "会社情報"
+}]
+
+for (var i = 0; i < data.length; i++) {
+    var obj = data[i];
+    for (var key in obj) {
+        if (!isInArray(KeyArray,key)) {
+            //console.log(key)
+            objects[obj['Identification key']] = obj[key];
+            fs.writeFileSync('lang/' + key + '.json', JSON.stringify(objects, null, 2), { encoding: 'utf8' }, function (err) {
+                if (err)
+                { throw err; }
+                console.log("completed")
+
+            });
+            
+        }
+    }
+
+}
